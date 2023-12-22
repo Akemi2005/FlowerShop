f300c91a764c051b3a7bbd34f137acca5a01a788
Akemi <124843821+Akemi2005@users.noreply.github.com>
2023-12-22 03:34:26 +0200

    Update script.js

--- Subscr/script.js
+++ Subscr/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  fetch('https://raw.githubusercontent.com/Akemi2005/FlowerShop/blob/main/plans.json')
+  fetch('https://raw.githubusercontent.com/Akemi2005/FlowerShop/main/plans.json')
   .then(response => response.json())
   .then(plans => {
     const planCardsContainer = document.getElementById('planCardsContainer');
